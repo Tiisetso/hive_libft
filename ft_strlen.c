5e9e45b165dfc7f78bb133a02bbac4375f27fd57
Tiisetso Murray <timurray@student.hive.fi>
2025-04-15 14:37:55 +0300

    strlen.

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,16 +6,18 @@
 /*   By: timurray <timurray@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 18:53:26 by timurray          #+#    #+#             */
-/*   Updated: 2025/04/15 12:27:26 by timurray         ###   ########.fr       */
+/*   Updated: 2025/04/15 14:35:35 by timurray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlen(char *str)
+#include <string.h>
+
+size_t	ft_strlen(const char *s)
 {
 	int	count;
 
 	count = 0;
-	while (*str++)
+	while (*s++)
 		count++;
 	return (count);
 }
