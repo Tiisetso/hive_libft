95d81fe88bd905e4e0c1392b90e789a5a81059af
Tiisetso Murray <timurray@student.hive.fi>
2025-04-15 09:36:15 +0300

    isalpha changes

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,18 +6,14 @@
 /*   By: timurray <timurray@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 15:40:40 by timurray          #+#    #+#             */
-/*   Updated: 2025/04/14 15:56:10 by timurray         ###   ########.fr       */
+/*   Updated: 2025/04/15 09:25:27 by timurray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-int	ft_isalpha(char c);
 
 int	ft_isalpha(char c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-	{
 		return (1);
-	}
 	return (0);
 }
 
