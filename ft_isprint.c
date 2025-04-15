14ebe6ae9e21b3745d37f05a44020d4ea4196c8d
Tiisetso Murray <timurray@student.hive.fi>
2025-04-15 14:31:58 +0300

    isprint

--- ft_isprint.c
+++ ft_isprint.c
@@ -6,10 +6,11 @@
 /*   By: timurray <timurray@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 12:06:28 by timurray          #+#    #+#             */
-/*   Updated: 2025/04/15 12:33:13 by timurray         ###   ########.fr       */
+/*   Updated: 2025/04/15 14:31:41 by timurray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_isprint(int c){
-return 0;	
+int	ft_isprint(int c)
+{
+	return (c >= 32 && c < 127);
 }
