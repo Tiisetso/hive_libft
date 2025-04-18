ae888f31d6a2cdc94e9d6511ab9fc5d4c48567e2
Tiisetso Daniel Murray <tiisetsomurray@gmail.com>
2025-04-18 12:28:33 +0300

    strlcpy.c basic.

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,16 +6,40 @@
 /*   By: timurray <timurray@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 15:11:35 by timurray          #+#    #+#             */
-/*   Updated: 2025/04/17 11:31:57 by timurray         ###   ########.fr       */
+/*   Updated: 2025/04/18 12:27:40 by timurray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 
-size_t ft_strlcpy(char *dst, const char *src, size_t size)
+size_t ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-	unsigned int count;
+	size_t count;
 
 	count = 0;
-	dst[count] = '\0';
+	while (*(src + count))
+		count++;
+	if (count == 0)
+		return (0);
+	while ((*src) && (dstsize > 0))
+	{
+		*dst++ = *src++;
+		dstsize--;
+	}
+	*dst = 0;
+	return (count);
 }
+
+// // TODO remove
+// #include <stdio.h>
+// int main(void)
+// {
+// 	size_t dstsize = 20;
+// 	char dst[dstsize];
+// 	char *src = "Hey from here";
+
+// 	ft_strlcpy(dst, src, dstsize);
+// 	printf("%s\n", src);
+// 	printf("%s\n", dst);
+// 	return (0);
+// }
