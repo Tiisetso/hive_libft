8b828bc2217051d1da76ecaca9f615f7fa6b4b48
Tiisetso Murray <timurray@student.hive.fi>
2025-05-02 10:15:13 +0300

    local changes.

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,12 +6,12 @@
 /*   By: timurray <timurray@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 15:11:35 by timurray          #+#    #+#             */
-/*   Updated: 2025/05/02 10:02:52 by timurray         ###   ########.fr       */
+/*   Updated: 2025/05/02 10:14:57 by timurray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
-#TODO ft_strlcpy
+
 size_t ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	unsigned int count;
