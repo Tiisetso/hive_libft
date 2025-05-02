c69c8bd9db32af56a23a013ab2731ca7599adef5
Tiisetso Murray <timurray@student.hive.fi>
2025-05-02 13:13:14 +0300

    strncmp.c

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: timurray <timurray@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 18:53:26 by timurray          #+#    #+#             */
-/*   Updated: 2025/04/22 20:07:36 by timurray         ###   ########.fr       */
+/*   Updated: 2025/05/02 12:54:17 by timurray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ size_t	ft_strlen(const char *s)
 	size_t	count;
 
 	count = 0;
-	while (*s++)
-		count++;
+	while (*(s + count))
+		count++; //TODO Check skipping past first index
 	return (count);
 }
---
+++ ft_strncmp.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: timurray <timurray@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/02 12:25:26 by timurray          #+#    #+#             */
+/*   Updated: 2025/05/02 13:12:52 by timurray         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+int ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t i;
+	size_t result;
+
+	i = 0;
+	result = 0;
+	while ((*s1 || *s2) && (i < n))
+	{
+		result = *s1++ - *s2++;
+		if (result != 0)
+			return result;
+		i++;
+	}
+	return (result);
+}
+
+// #include <stdio.h>
+// int main (void)
+// {
+// 	const char *s1 = "hello";
+// 	const char *s2 = "hellow";
+// 	size_t n = 4;
+// 	printf("%i",ft_strncmp(s1,s2,n));
+// 	return (0);
+// }
