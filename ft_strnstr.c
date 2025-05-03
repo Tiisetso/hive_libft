1e7d2a0adb13f48da7899c25c25cb27a3fe3a4cc
Tiisetso Murray <timurray@student.hive.fi>
2025-05-03 11:52:57 +0300

    strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,26 +6,37 @@
 /*   By: timurray <timurray@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 15:44:09 by timurray          #+#    #+#             */
-/*   Updated: 2025/05/02 18:24:07 by timurray         ###   ########.fr       */
+/*   Updated: 2025/05/03 11:52:03 by timurray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
+#include "libft.h"
 
-char *ft_strnstr(const char *big, const char *little, size_t len)
+char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-	unsigned int i;
-	unsigned int j;
+	unsigned	int i;
+	unsigned	int j;
+	char		*location;
 
 	i = 0;
-	while ((*(big + i)) && (len > 0))
+	location = NULL;
+	if (strlen(little) == 0)
+		return (big);
+	while ((*(big + i)) && (len-- > 0))
 	{
 		j = 0;
 		if (*(big + i) == *(little + j))
 		{
 			while (*(big + i + j) == *(little + j))
 				j++;
-			if 
-		}		
+			if (*(little + j) == '\0')
+			{
+				location = big + i;
+				break;
+			}
+		}
+		i++;
 	}
+	return (location);
 }
