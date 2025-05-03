522c5d361162743d5df78322809bfc70b9fa5e43
Tiisetso Murray <timurray@student.hive.fi>
2025-05-03 12:30:09 +0300

    checkpoint.

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: timurray <timurray@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 12:29:27 by timurray          #+#    #+#             */
-/*   Updated: 2025/04/22 20:07:22 by timurray         ###   ########.fr       */
+/*   Updated: 2025/05/03 12:27:49 by timurray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,17 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	size_t	length;
+	int		src_length;
+	int		dst_length;
+	int		src_index;
+	size_t	dst_offset;
 
-	length = ft_strlen(src);
-	return (length);
+	src_length = ft_strlen(src);
+	dst_length = ft_strlen(dst);
+	dst_offset = dst_length;
+	src_index = 0;
+	while (*(src + src_index) && (dst_offset < dstsize))
+		*(dst + dst_offset++) = *(src + src_index++);
+	*(dst + dst_offset) = '\0';
+	return (src_length + dst_length);
 }
