04d6c2b6b0e174c0c77a3e1731ebd9a2c63f7d31
Tiisetso Murray <timurray@student.hive.fi>
2025-05-03 14:50:27 +0300

    calloc

---
+++ ft_calloc.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: timurray <timurray@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/03 14:09:55 by timurray          #+#    #+#             */
+/*   Updated: 2025/05/03 14:48:04 by timurray         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "stdlib.h"
+#include "stdint.h"
+
+void *calloc(size_t nmemb, size_t size)
+{
+	void *ptr;
+
+	if ((nmemb == 0 || size == 0) || (nmemb > SIZE_MAX / size))
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (!ptr)
+		return (NULL);
+	ft_bzero(ptr, nmemb * size);
+	
+	return (ptr);
+}
