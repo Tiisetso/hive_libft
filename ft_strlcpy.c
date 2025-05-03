342e068dcc9a9fbb18dba9896d08e6ece25591c4
Tiisetso Murray <timurray@student.hive.fi>
2025-05-03 11:55:52 +0300

    strlcpy.c

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,19 +6,16 @@
 /*   By: timurray <timurray@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 15:11:35 by timurray          #+#    #+#             */
-/*   Updated: 2025/05/02 10:15:23 by timurray         ###   ########.fr       */
+/*   Updated: 2025/05/03 11:54:13 by timurray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 #include "libft.h"
 
-size_t	ft_strlen(const char *s);
-
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t length;
-
 
 	length = ft_strlen(src);
 	if (length == 0)
